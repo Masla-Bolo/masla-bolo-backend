a13169aac8287c18634adeee815753b8f6f57a37
Demaurr <69788671+Demaurr@users.noreply.github.com>
2024-09-01 21:24:31 +0500

    Uncommented createsuperuser and set the is_active to True

--- users/models.py
+++ users/models.py
@@ -6,45 +6,27 @@ class MyApiUserManager(BaseUserManager):
         if not email:
             raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email), username=username)
-        user.set_password(password)
+        user.set_password(password)  # This hashes the password
         user.save(using=self._db)
         return user
 
-    # def create_superuser(self, email, username, password):
-    #     user = self.create_user(email, username, password)
-    #     user.is_staff = True
-    #     user.is_superuser = True
-    #     user.save(using=self._db)
-    #     return user
+    def create_superuser(self, email, username, password):
+        user = self.create_user(email, username, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 class MyApiUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=255, unique=True)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = MyApiUserManager()
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-
-    # Add related_name arguments to resolve the clash
-    groups = models.ManyToManyField(
-        'auth.Group',
-        verbose_name='groups',
-        blank=True,
-        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
-        related_name='myapiuser_set',
-        related_query_name='myapiuser',
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        verbose_name='user permissions',
-        blank=True,
-        help_text='Specific permissions for this user.',
-        related_name='myapiuser_set',
-        related_query_name='myapiuser',
-    )
 
     def __str__(self):
         return self.email
