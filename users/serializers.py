7105ef20ee2ccef15ab4e8cba14b52f6d1677cc5
Demaurr <69788671+Demaurr@users.noreply.github.com>
2024-09-01 21:26:59 +0500

    Added class based serializers for Login, Register and Main MyApiUser

--- users/serializers.py
+++ users/serializers.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
 from .models import MyApiUser
+from django.contrib.auth import authenticate
 
-class UserSerializer(serializers.ModelSerializer):
+class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = MyApiUser
-        fields = ['id', 'email', 'username', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ["id", 'email', "username", 'password']
 
     def create(self, validated_data):
         user = MyApiUser.objects.create_user(
@@ -15,8 +15,20 @@ class UserSerializer(serializers.ModelSerializer):
         )
         return user
 
-    def update(self, instance, validated_data):
-        if 'password' in validated_data:
-            password = validated_data.pop('password')
-            instance.set_password(password)
-        return super().update(instance, validated_data)
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        email = data.get("email")
+        password = data.get("password")
+        user = authenticate(username=email, password=password)
+        if user is None:
+            raise serializers.ValidationError("Invalid login credentials")
+        return data
+
+# Serializer for listing user details
+class MyApiUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MyApiUser
+        fields = ['id', 'email', 'username', 'is_active']  # Adjust fields as necessary
