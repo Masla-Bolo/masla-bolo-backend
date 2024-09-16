5e333fce754e5abee1dc96e3b65fb8c102154c6c
Asher <asheressani@gmail.com>
2024-09-17 04:00:17 +0500

    Fixed folder naming conventions

--- my_site/urls.py
+++ my_site/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for crud_project project.
+URL configuration for my_site project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -20,7 +20,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
+    path('api/', include('my_api.urls')),
 ]
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
